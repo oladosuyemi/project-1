f8d2712d49425d3c6ece3ec981a0f08d1abeb6d0
oladosuyemi <oladosuyemi@gmail.com>
2023-03-03 12:43:13 -0500

    providers file

---
+++ providers.tf
@@ -0,0 +1,46 @@
+# Azure Provider
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.57.0"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.46.0"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "2.36.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+  features {}
+}
+
+
+provider "azurerm" {
+  # Configuration options
+  region = "us-east-1"
+  features {}
+}
+
+provider "azuread" {
+  # Configuration options
+  region = "us-east-1"
+  features {}
+}
